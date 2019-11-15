20f28b815dcfe1545b2c1d2020f5ef2f2ffad706
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2019-11-15 12:34:13 +0100

    Update runme.ps1
    do it 5 times to even out irregularies

--- runme.ps1
+++ runme.ps1
@@ -1,6 +1,9 @@
 $msbuildexe = "c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
 
-$x = &$msbuildexe AntiVirusBench.sln  /t:Clean
-
-Measure-Command { &$msbuildexe AntiVirusBench.sln  /t:Build }
+Measure-Command { 
+    For ($i=0; $i -le 5; $i++) {
+        &$msbuildexe AntiVirusBench.sln  /t:Clean
+        &$msbuildexe AntiVirusBench.sln  /t:Build
+    }
+}
